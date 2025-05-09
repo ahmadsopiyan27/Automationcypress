78b06d3ae6f6db466d3c7d8f26f246a4fd8b10d7
ahmadsopiyan27 <ahmmadsopiyann@gmail.com>
2025-05-09 21:45:37 +0700

    start cypress

---
+++ 1.cy.js
@@ -0,0 +1,7 @@
+describe('UI automation cypress', async function () {
+    it('login', async function () {
+        cy.visit('https://opensource-demo.orangehrmlive.com/')
+        
+    })
+    
+})
