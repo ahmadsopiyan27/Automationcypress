effda1e4705b7c3d8cf3d1153309ceeea2c869c4
ahmadsopiyan27 <ahmmadsopiyann@gmail.com>
2025-05-09 23:19:35 +0700

    update

--- 1.cy.js
+++ 1.cy.js
@@ -1,9 +1,12 @@
 describe('UI automation cypress', async function () {
     it('login', async function () {
       await cy.visit('https://opensource-demo.orangehrmlive.com/')
-       await cy.get(`cy.get("[name='username']`)
-    //    await cy.get(`cy.get("[name='password']`)
-    cy.xpath("//input[@name='password']")
+      await cy.get(`[name='username']`).type('Admin');
+    let password = cy.get("[name='password']");
+    await password.type('admin')
+    await password.clear();
+    await password.type('admin123');
+    await cy.get('button').click();
         
     });
     
