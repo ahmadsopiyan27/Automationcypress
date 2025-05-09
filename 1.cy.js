8c6935d23ba78b05f0f97546b7d2dd4c1cd2cf99
ahmadsopiyan27 <ahmmadsopiyann@gmail.com>
2025-05-09 22:49:59 +0700

    update

--- 1.cy.js
+++ 1.cy.js
@@ -1,7 +1,10 @@
 describe('UI automation cypress', async function () {
     it('login', async function () {
-        cy.visit('https://opensource-demo.orangehrmlive.com/')
+      await cy.visit('https://opensource-demo.orangehrmlive.com/')
+       await cy.get(`cy.get("[name='username']`)
+    //    await cy.get(`cy.get("[name='password']`)
+    cy.xpath("//input[@name='password']")
         
-    })
+    });
     
-})
+});
