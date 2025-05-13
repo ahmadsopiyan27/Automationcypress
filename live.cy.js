cd9a93fae21e8f55de6673438679a58bd865b93d
ahmadsopiyan27 <ahmmadsopiyann@gmail.com>
2025-05-13 21:31:39 +0700

    update

--- live.cy.js
+++ live.cy.js
@@ -1,7 +1,10 @@
 describe('day 6', async() => {
     it('static dropdown', async  () => {
+        cy.on('uncaught:exception', (err, runnable) => {
+            return false; // Mengabaikan error khusus untuk tes ini
+        });
         cy.visit('https://demo.automationtesting.in/Register.html')
-        cy.xpath(`//select[@id='Skills']`);
+        cy.xpath(`//select[@id='Skills']`).select(5);
 
         
     })
