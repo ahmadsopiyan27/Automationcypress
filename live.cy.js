da33b5d3686de1936cab399b60024c467e108c0b
ahmadsopiyan27 <ahmmadsopiyann@gmail.com>
2025-05-19 20:53:52 +0700

    handle dropdown

--- live.cy.js
+++ live.cy.js
@@ -30,13 +30,29 @@ describe('day 6', async() => {
         // await expect(optionArray).to.include(expectedSkills)
   });
   /////// this is update
-  it('dyanmic looping', async  () => {
+  it.skip('dyanmic looping', async  () => {
         cy.on('uncaught:exception', (err, runnable) => {
             return false; // Mengabaikan error khusus untuk tes ini
         });
     await cy.visit('https://demo.automationtesting.in/Register.html');
     await cy.wait(3000);
     await cy.get('#Skills').select('C')
+    
+  });
+  it('autosuggestive dropdown', async function () {
+     cy.on('uncaught:exception', (err, runnable) => {
+            return false; // Mengabaikan error khusus untuk tes ini
+        });
+
+    let fieldSuggestive = `//span[@class='select2-selection select2-selection--single']`;
+    let fieldSearch = `//input[@class='select2-search__field']`
+    let fieldSearchItem = `//ul[@class='select2-results__options']/li`
+
+    cy.visit('https://demo.automationtesting.in/Register.html');
+    await cy.xpath(fieldSuggestive).click();
+    await cy.xpath(fieldSearch).type('de');
+    await cy.xpath(fieldSearchItem).contains('Bangladesh').click();
+
     
   })
 });
